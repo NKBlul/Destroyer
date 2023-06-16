7b4213778513a9e39fda2dfc75006c473e87578b
HansenHarly <hansenharly123@gmail.com>
2023-06-16 11:05:38 -0400

    Fixed multiplayer jankiness movmeent
    - Baked better navmesh for ai chase
    
    - Fixed janky momvement on multiplayer
    
    - Disabled camera on ship

--- Assets/Scripts/Base Scripts/BaseEnemy.cs
+++ Assets/Scripts/Base Scripts/BaseEnemy.cs
@@ -1,16 +1,32 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseEnemy : BaseCharacter
 {
+    Rigidbody rb;
+    PhotonView pv;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        pv = GetComponent<PhotonView>();
+    }
+
     private void Start()
     {
-
+        if (!pv.IsMine)
+        {
+            Destroy(rb);
+        }
     }
 
     private void Update()
     {
-        
+        if (!pv.IsMine)
+        {
+            return;
+        }
     }
 }
--- Assets/Scripts/Base Scripts/BasePlayer.cs
+++ Assets/Scripts/Base Scripts/BasePlayer.cs
@@ -1,17 +1,33 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BasePlayer : BaseCharacter
 {
+    Rigidbody rb;
+    PhotonView pv;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        pv = GetComponent<PhotonView>();
+    }
+
     private void Start()
     {
-        
+        if (!pv.IsMine)
+        {
+            Destroy(rb);
+        }
     }
 
     private void Update()
     { 
-
+        if (!pv.IsMine)
+        {
+            return;
+        }
     }
 
    
