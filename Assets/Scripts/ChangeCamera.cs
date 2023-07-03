cfb7bcb0481fd10f8c8aad383c9708fd97ec6941
mohamadMZ98 <mhd.mzafar.mm@gmail.com>
2023-07-02 21:44:01 -0400

    made a change camera script
    the script will make the camera change from the main camera to the turret camera if you press T

---
+++ Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public Camera[] cameras;  // Array to hold your cameras
+    private int currentCameraIndex;  // Index of the current active camera
+
+    private void Start()
+    {
+        // Set the first camera as the starting camera
+        currentCameraIndex = 0;
+        ActivateCamera(currentCameraIndex);
+    }
+
+    private void Update()
+    {
+        // Check for key input to switch cameras
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SwitchCamera();
+        }
+    }
+
+    public void SwitchCamera()
+    {
+        // Deactivate the current camera
+        cameras[currentCameraIndex].gameObject.SetActive(false);
+
+        // Increment the camera index
+        currentCameraIndex++;
+
+        // Wrap around to the first camera if the index goes beyond the array size
+        if (currentCameraIndex >= cameras.Length)
+            currentCameraIndex = 0;
+
+        // Activate the new camera
+        ActivateCamera(currentCameraIndex);
+    }
+
+    private void ActivateCamera(int index)
+    {
+        // Activate the specified camera
+        cameras[index].gameObject.SetActive(true);
+    }
+}
+
