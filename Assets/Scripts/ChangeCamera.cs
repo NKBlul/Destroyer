6fead3cb4affc015b694553388efe04c161fc8d6
mohamadMZ98 <mhd.mzafar.mm@gmail.com>
2023-07-09 23:01:27 -0400

    player can stick on the ship again
    the player can move along the ship no matter where he is.
    Tried to work on disabling player movement when you switch to turret view or ship view but didn't work :(

--- Assets/Scripts/ChangeCamera.cs
+++ Assets/Scripts/ChangeCamera.cs
@@ -7,9 +7,13 @@ public class ChangeCamera : MonoBehaviour
     public Camera[] cameras;  // Array to hold your cameras
     private int currentCameraIndex;  // Index of the current active camera
 
+    //public GameObject turret;
+    //public GameObject player;
+    //public GameObject ship;
     MainTurret mainTurret;
 
-   
+
+
     private void Start()
     {
         // Set the first camera as the starting camera
@@ -17,6 +21,8 @@ public class ChangeCamera : MonoBehaviour
         ActivateCamera(currentCameraIndex);
         
         mainTurret = FindObjectOfType<MainTurret>();
+        //player = FindAnyObjectByType<PlayerController>();
+        //ship = FindObjectOfType<MoveShip>();
     }
 
     private void Update()
@@ -27,6 +33,7 @@ public class ChangeCamera : MonoBehaviour
         {
             SwitchCamera();
         }
+
     }
 
     public void SwitchCamera()
@@ -52,16 +59,85 @@ public class ChangeCamera : MonoBehaviour
         if (cameras[index].gameObject.tag == "turret" && mainTurret != null)
         {
             mainTurret.GetComponent<MainTurret>().SetActive();
-            
-            cameras[index].enabled= true;
+
+            cameras[index].enabled = true;
             Camera.main.enabled = false;
         }
         else if (!(cameras[index].gameObject.tag == "turret") && mainTurret != null)
         {
             mainTurret.GetComponent<MainTurret>().SetNotActive();
-            cameras[index].enabled= false;
+            cameras[index].enabled = false;
             Camera.main.enabled = true;
         }
+
+        //if ((currentCameraIndex == 0) && player != null && turret != null && ship != null)
+        //{
+        //    player.GetComponent<PlayerController>().enabled = true;
+        //    turret.GetComponent<MainTurret>().enabled = false;
+        //    ship.GetComponent<MoveShip>().enabled = false;
+        //}
+
+        //else if ((currentCameraIndex == 1) && player != null && turret != null && ship != null)
+        //{
+        //    player.GetComponent<PlayerController>().enabled = false;
+        //    turret.GetComponent<MainTurret>().enabled = false;
+        //    ship.GetComponent<MoveShip>().enabled = true;
+        //}
+
+        //else if ((currentCameraIndex == 2) && player != null && turret != null && ship != null)
+        //{
+        //    player.GetComponent<PlayerController>().enabled = false;
+        //    turret.GetComponent<MainTurret>().enabled = true;
+        //    ship.GetComponent<MoveShip>().enabled = false;
+        //}
+
+        //if (index == 2) // Assuming the main turret camera is at index 2
+        //{
+        //    // Disable player movement script
+        //    if (player != null)
+        //    {
+        //        PlayerController playerMovement = player.GetComponent<PlayerController>();
+        //        if (playerMovement != null)
+        //        {
+        //            playerMovement.enabled = false;
+        //        }
+        //    }
+
+        //    // Disable ship movement script
+        //    if (ship != null)
+        //    {
+        //        MoveShip shipMovement = ship.GetComponent<MoveShip>();
+        //        if (shipMovement != null)
+        //        {
+        //            shipMovement.enabled = false;
+        //        }
+        //    }
+        //}
+        //else
+        //{
+        //    // Enable player movement script
+        //    if (player != null)
+        //    {
+        //        PlayerController playerMovement = player.GetComponent<PlayerController>();
+        //        if (playerMovement != null)
+        //        {
+        //            playerMovement.enabled = true;
+        //        }
+        //    }
+
+        //    // Enable ship movement script
+        //    if (ship != null)
+        //    {
+        //        MoveShip shipMovement = ship.GetComponent<MoveShip>();
+        //        if (shipMovement != null)
+        //        {
+        //            shipMovement.enabled = true;
+        //        }
+        //    }
+        //}
+
+
     }
 }
+
 
