10f99210e00997548ef16b5669c97ce011fdf370
HansenHarly <hansenharly123@gmail.com>
2023-08-27 11:41:33 -0500

    readjusted more stuffs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -158,7 +158,7 @@ public class PlayerController : MonoBehaviourPunCallbacks/*, IDamageable*/
             //killed everyone
             Debug.Log("Ded");
             Launcher.Instance.Win();
-            //SceneManager.LoadScene(2);
+            SceneManager.LoadScene(2);
         }
     }
 
